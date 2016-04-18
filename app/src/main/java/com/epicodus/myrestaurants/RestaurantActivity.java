319c87474ff315decc42120a2220ff39aeaf26c3
Chris Houghton and David Remington <cehoughton71@gmail.com>
2016-04-18 16:26:42 -0700

    Add testing

---
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantActivity.java
@@ -0,0 +1,22 @@
+package com.epicodus.myrestaurants;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RestaurantActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the locations near:" + location);
+    }
+}
